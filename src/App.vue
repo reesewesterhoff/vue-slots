14679bc0c2d34ea439e604e525069defd0eb5773
reesewesterhoff <39221548+reesewesterhoff@users.noreply.github.com>
2018-12-06 12:41:06 -0600

    finish slots

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <!-- import form helper -->
+    <!-- pass in unique info from parent component to fill slots -->
+    <form-helper v-bind:title="title">
+      <div slot="form-header">
+        <h3>Form Title</h3>
+        <p>Form info</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="username" required />
+        <input type="text" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+import FormHelper from './components/FormHelper.vue';
+
+export default {
+  components: {
+    'form-helper': FormHelper,
+  },
+  data () {
+    return {
+      title: 'I am a dynamic slot title'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/FormHelper.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+      <!-- create slots for things like forms so that any time there is a form -->
+      <!-- you can insert different info into the pre made areas -->
+      <h1>Please fill out our form...</h1>
+      <h2>{{title}}</h2>
+      <form>
+          <div id="form-header">
+              <slot name="form-header"></slot>
+          </div>
+          <div id="form-fields">
+              <slot name="form-fields"></slot>
+          </div>
+          <div id="form-controls">
+              <slot name="form-controls"></slot>
+          </div>
+          <div id="useful-links">
+              <ul>
+                  <li><a href="#">Link 1</a></li>
+                  <li><a href="#">Link 2</a></li>
+                  <li><a href="#">Link 3</a></li>
+                  <li><a href="#">Link 4</a></li>
+              </ul>
+          </div>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String
+        }
+    },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2{
+    text-align: center;
+}
+form{
+    width: 100%;
+    max-width: 960px;
+    margin: 0 auto;
+}
+#useful-links ul{
+    padding: 0;
+}
+#useful-links li{
+    display: inline-block;
+    margin-right: 10px;
+}
+form > div{
+    padding: 20px;
+    background: #eee;
+    margin: 20px 0;
+}
+#form-header{
+    background: #ddd;
+    border: 1px solid #bbb;
+}
+</style>
